bbd4d8d50565da471dec5459688873a277e9b336
clebars <lebac536@newschool.edu>
2022-03-07 21:50:27 -0500

    asdfg
    asdfg

--- projects/Project 2/FINAL/script.js
+++ projects/Project 2/FINAL/script.js
@@ -349,23 +349,4 @@ $(document).ready(function(){
   })  
 
 
-
-  document.addEventListener( "DOMContentLoaded", function() {
-    // it's a div, that holds your news
-    // it holds ul with news in li elements
-    var div = document.getElementById( "credits" );
-    div.addEventListener( "scroll", function() {
-      var max_scroll = this.scrollHeight - this.clientHeight;
-      var current_scroll = this.scrollTop;
-      var bottom = 100;
-      if ( current_scroll + bottom >= max_scroll ) {
-        var ul = document.getElementsByTagName( "ul" )[ 0 ];
-        var current = parseInt( ul.dataset.current, 10 );
-        var li = document.getElementsByTagName( "li" )[ current ];
-        var new_li = li.cloneNode( true );
-        ul.appendChild( new_li );
-        ul.dataset.current = current + 1;
-      }
-    } );
-  } );
   
